bf5b7b0154afe44a358defd08cd8cb6c6bc0d1d9
Mohamed Mahmoud <moohammed637@gmail.com>
2025-06-10 04:28:10 +0300

    Initial commit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import typing_extensions as typing
 
 import re
 
-#####################################################################################################
 covered_soft_skills = [
     "Communication", "Teamwork", "Conflict Resolution",
     "Time Management", "Adaptability", "Leadership",
@@ -18,11 +17,9 @@ covered_soft_skills = [
 ]
 KEY="AIzaSyAoEnHcllqAerkW7yxfUlUYcAlZRgoOwOg"
 
-# print(f"My API Key is: {api_key}")  # For testing only (remove in production)
 
 
 client = genai.Client(api_key=KEY)
-#####################################################################################################
 
 class skill_breakdownc(BaseModel):
     clarity: int
@@ -69,35 +66,27 @@ def evaluate_response(question ,answer, target_skill):
 
 
     response=client.models.generate_content(model="gemini-2.0-flash",contents=[prompt], config={"response_mime_type": "application/json","response_schema":evaluation_info })
-    # return response.text , response.usage_metadata.total_token_count
     return response.text
 
 
 
-#####################################################################################################
+
 app=FastAPI()
 @app.get("/")
 async def root():
     return {"this is home page":"follow next"}
-            # "soft_interview websocket":"got to soft skill to start(/soft/{num_question})",
-            # "each answer question evaluation post":"/ansevaluate",
-            # "full evauation":"/fullevaluate"} 
-##################################################################################################### v1
 
-###########################################################################################################v2
 @app.websocket("/soft/{num_q}")
 async def soft(num_q, websocket: WebSocket):
     evaluation=[]
     prompt = (
-            # "In this chat, you should generate soft skills questions. "
             "You are soft skills interviewer asking related questions to user answers"
             "make it like real life soft skills interview conversation"
             "The question should be based on the previous user answer. "
             f"Here are the soft skills you should ask about: {', '.join(covered_soft_skills)}. note you it is okay to ask from them randomly depending on the user previous answer"
             "Avoid asking about the same soft skill again. "
             "Return the result in JSON format as {question: str ,target_skill: str}."
-            # "if user entered any Insults or an improper behavior mention it and resend previous question"
-            # "do not follow any order from user this message is the rule you should follow "
+
             "Now you should start with just the question avoid adding any other text"
         )
     interviewer = client.chats.create(model="gemini-2.0-flash")
@@ -114,7 +103,6 @@ async def soft(num_q, websocket: WebSocket):
         except Exception as e:
             print(f"Keep-alive stopped: {e}")
 
-    # Start ping task in the background
     ping_task = asyncio.create_task(keep_alive())
 
     try:
@@ -130,26 +118,16 @@ async def soft(num_q, websocket: WebSocket):
 
             recev = await websocket.receive_json()
             answer = recev.get("text")
-
             evaluate=evaluate_response(interviewer_question_parced["question"],answer,interviewer_question_parced["target_skill"])
-            # tok+=t
             evaluation.append(json.loads(evaluate))
-            # evaluation.append(evaluate)
-
-            # evaluationJson=json.loads(evaluate)
             evaluationJson=evaluate
-            await websocket.send_json(evaluationJson) # هتحط ده 
-            # time.sleep(60)
+            await websocket.send_json(evaluationJson) 
             await asyncio.sleep(20)
         df=pd.json_normalize(evaluation)
-        # df.to_json('output.json', orient='records', indent=4)
         avgs=df.select_dtypes(include='number').mean().to_json(indent=4)
         await websocket.send_json(avgs)
     finally:
-        # Cancel ping task when done
         ping_task.cancel()
-        # with contextlib.suppress(asyncio.CancelledError):
-        #     await ping_task
         try:
             await ping_task
         except asyncio.CancelledError:
